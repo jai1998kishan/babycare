3b5ab8fefedef388b0f34471ac34de556a0b2785
hello-mahi <jaikishang724@gmail.com>
2025-03-07 21:02:20 +0530

    07-03-2025

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -4,6 +4,7 @@ async function home(req, res) {
   let data = await User.findOne({ _id: req.session.userid });
   res.render("admin/home", {
     title: "Admin",
+
     data: data,
     session: req.session,
   });
